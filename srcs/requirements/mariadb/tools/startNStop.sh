55c77f035d810ed95ad8d382a4684239aae0b9bd
ayyshaaa <stierlen_aicha@hotmail.fr>
2025-11-27 17:05:07 +0100

    finished entrypoint script for mariadb init and run

---
+++ srcs/requirements/mariadb/tools/startNStop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+signal_terminate() {
+	mariadb-admin shutdown &
+	wait $!
+	echo "Mariadb shutdown complete."
+}
+
+# SIGTERM is default signal sent by Docker when stopping a container
+trap "signal_terminate" SIGTERM
+
+# Initialize MariaDB if not already done
+
+echo "Initializing MariaDB database..."
+mariadb-install-db \
+	--auth-root-authentication-method=normal
+echo "MariaDB initialization completed."
+
+echo "Starting MariaDB daemon..."
+# Replace current process with mariadb daemon
+# This keeps the container running until MariaDB is stopped
+exec mariadbd &
+wait $!
+exit 1 
+
