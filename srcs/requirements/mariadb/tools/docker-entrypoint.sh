414e48fb1c26f7d3c0a0b3f70c3176d7644cfb17
ayyshaaa <stierlen_aicha@hotmail.fr>
2025-11-30 18:14:09 +0100

    fixed env var expansion + first co with maridbd-admin with socket before pwd init

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 signal_terminate() {
     echo "Received SIGTERM, shutting down MariaDB..."
     if [ -n "$MARIADB_PID" ]; then
-        mariadb-admin shutdown 2>/dev/null || \
+        mariadb-admin -u root --password=$MYSQL_ROOT_PASSWORD shutdown 2>/dev/null || \
         kill -TERM "$MARIADB_PID"
         wait "$MARIADB_PID"
     fi
@@ -15,27 +15,27 @@ signal_terminate() {
 trap "signal_terminate" SIGTERM SIGINT
 
 # Initialize MariaDB if data directory is empty
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+if [ ! -d "/home/$MYSQL_USER/data/mysql" ]; then
     echo "Initializing MariaDB database..."
-    chown -R mysql:mysql /var/lib/mysql /var/log/mysql /var/run/mysqld
-    chmod 755 /var/lib/mysql /var/log/mysql /var/run/mysqld  
-    mariadb-install-db --user=mysql
+    chown -R $MYSQL_USER:$MYSQL_USER /home/$MYSQL_USER/data /var/log/mysql /var/run/mysqld
+    chmod 755 /home/$MYSQL_USER/data /var/log/mysql /var/run/mysqld
+    mariadb-install-db --user=$MYSQL_USER
     if [ -d "/docker-entrypoint-initdb.d" ] && [ "$(ls -A /docker-entrypoint-initdb.d 2>/dev/null)" ]; then
-        mariadbd --user=mysql --skip-networking --socket=/var/run/mysqld/mysqld.sock &
+        mariadbd --user=$MYSQL_USER --skip-networking --socket=/var/run/mysqld/mysqld.sock &
     	temp_pid=$!
-		sleep 1
-		chmod +x /docker-entrypoint-initdb.d/01-init-database.sh
-		/docker-entrypoint-initdb.d/01-init-database.sh
-		echo "Initialization complete."
-		mariadb-admin -u root --socket=/var/run/mysqld/mysqld.sock shutdown
-		wait $temp_pid
+	sleep 1
+	chmod +x /docker-entrypoint-initdb.d/01-init-database.sh
+	/docker-entrypoint-initdb.d/01-init-database.sh
+	echo "Initialization complete."
+	mariadb-admin -u root --socket=/var/run/mysqld/mysqld.sock --password=$MYSQL_ROOT_PASSWORD shutdown
+	wait $temp_pid
     fi
 else
     echo "MariaDB data directory exists, skipping initialization."
-    chown -R mysql:mysql /var/lib/mysql /var/log/mysql /var/run/mysqld
+    chown -R $MYSQL_USER:$MYSQL_USER /home/$MYSQL_USER/data /var/log/mysql /var/run/mysqld
 fi
 
 echo "Starting MariaDB..."
-exec mariadbd --user=mysql &
+mariadbd --user=$MYSQL_USER &
 MARIADB_PID=$!
 wait $MARIADB_PID
