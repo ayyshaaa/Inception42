60c126b5f762dad6ea64d284cb41a47badc8b079
ayyshaaa <stierlen_aicha@hotmail.fr>
2025-12-08 13:55:24 +0100

    check if file already exist before dl wp, cant connect to db yet

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -2,22 +2,31 @@
 
 set -e
 
-wp core download --path=/var/www/html --allow-root
-wp config create --path=/var/www/html --dbname="$WORDPRESS_DB_NAME" \
-    --dbuser="$WORDPRESS_DB_USER" \
-    --dbpass="$WORDPRESS_DB_PASSWORD" \
-    --dbhost="$WORDPRESS_DB_HOST" \
-    --allow-root
-wp core install --path=/var/www/html \
-    --url="http://$DOMAIN_NAME" \
-    --title="Inception" \
-    --admin_user="$WORDPRESS_ADMIN_NAME" \
-    --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
-    --admin_email="$WORDPRESS_ADMIN_EMAIL" \
-    --locale="fr_FR" \
-    --skip-email \
-    --allow-root
-wp user create "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
-    --user_pass="$WORDPRESS_USER_PASSWORD" --role="author" --allow-root
+if [ ! -f /var/www/html/wp-config.php ]; then
+	php -d memory_limit=256M /usr/local/bin/wp core download --allow-root
+
+	wp config create --dbname="$WORDPRESS_DB_NAME" \
+   		--dbuser="$WORDPRESS_DB_USER" \
+		--dbpass="$WORDPRESS_DB_PASSWORD" \
+		--dbhost="$WORDPRESS_DB_HOST:3306" \
+		--locale="fr_FR" \
+		--allow-root
+
+	wp core install --url="http://$DOMAIN_NAME" \
+		--title="Inception" \
+		--admin_user="$WORDPRESS_ADMIN_NAME" \
+		--admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+		--admin_email="$WORDPRESS_ADMIN_EMAIL" \
+		--skip-email \
+		--allow-root
+
+	wp user create "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
+		--user_pass="$WORDPRESS_USER_PASSWORD" \
+		--role="author" \
+		--allow-root
+
+	wp language core install fr_FR --allow-root
+	wp site switch-language fr_FR --allow-root
+fi
 
 chown -R www-data:www-data /var/www/html
