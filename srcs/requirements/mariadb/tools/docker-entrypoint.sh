1e235ec5f166ba0c3328029207e685972b3fb034
ayyshaaa <stierlen_aicha@hotmail.fr>
2025-12-08 18:22:58 +0100

    assessing mariadb readiness with 30s ping loop before calling init_db script

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -6,16 +6,25 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing MariaDB database..."
     chown -R mysql:mysql /var/lib/mysql /var/log/mysql /var/run/mysqld
     chmod 755 /var/lib/mysql /var/log/mysql /var/run/mysqld
-    mariadb-install-db --user=$MYSQL_USER
+    mariadb-install-db --user="$MYSQL_USER" --datadir=/var/lib/mysql
+    
     if [ -d "/docker-entrypoint-initdb.d" ] && [ "$(ls -A /docker-entrypoint-initdb.d 2>/dev/null)" ]; then
-        mariadbd --user=$MYSQL_USER --skip-networking --socket=/var/run/mysqld/mysqld.sock &
-    	temp_pid=$!
-	sleep 1
-	chmod +x /docker-entrypoint-initdb.d/01-init-database.sh
-	/docker-entrypoint-initdb.d/01-init-database.sh
-	echo "Initialization complete."
-	mariadb-admin -u root --socket=/var/run/mysqld/mysqld.sock --password=$MYSQL_ROOT_PASSWORD shutdown
-	wait $temp_pid
+        mariadbd --user="$MYSQL_USER" --skip-networking --socket=/var/run/mysqld/mysqld.sock --datadir=/var/lib/mysql &
+        mariadb_pid=$!
+	echo "Waiting for MariaDB socket..."
+	for i in {1...30}; do
+		if mariadb-admin -u root --socket=/var/run/mysqld/mysqld.sock ping 2>/dev/null; then
+			echo "MariaDB is ready"
+			break;
+		fi
+		sleep 1
+	done
+        chmod +x /docker-entrypoint-initdb.d/01-init-database.sh
+        /docker-entrypoint-initdb.d/01-init-database.sh
+        echo "Initialization scripts complete."
+        mariadb-admin -u root --password="$MYSQL_ROOT_PASSWORD" --socket=/var/run/mysqld/mysqld.sock shutdown 2>/dev/null || true
+        wait $mariadb_pid 2>/dev/null || true
+        sleep 2
     fi
 else
     echo "MariaDB data directory exists, skipping initialization."
@@ -23,4 +32,5 @@ else
 fi
 
 echo "Starting MariaDB..."
+echo "Executing: $@"
 exec "$@"
